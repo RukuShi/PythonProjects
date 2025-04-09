5cda5c852a087b62e3b3e4af998954c6cacd6c0d
RukuShi <beveryafraid1987@gmail.com>
2025-04-09 20:09:40 +0300

    trainer_name

--- Tests/test_pokemons.py
+++ Tests/test_pokemons.py
@@ -7,15 +7,9 @@ HEADER = {'Content - Type' : 'application/json', 'trainer_token':TOKEN}
 TRAINER_ID = '28848'
 
 def test_status_code():
-    response = requests.get(url =f'{URL}/pokemons', params = {'trainer_id': TRAINER_ID})
+    response = requests.get(url =f'{URL}/trainers', params = {'trainer_id': TRAINER_ID})
     assert response.status_code == 200
     
 def test_part_of_response():
-    response_get = requests.get(url =f'{URL}/pokemons', params = {'trainer_id': TRAINER_ID})   
-    assert response_get.json()["data"][0]["name"] == 'New Name'
-   
-
-@pytest.mark.parametrize('key, value', [('name', 'New Name'), ('trainer_id', TRAINER_ID), ('id', '284020')])
-def test_parametrize(key, value):
-    response_parametrize = requests.get(url =f'{URL}/pokemons', params = {'trainer_id': TRAINER_ID})
-    assert response_parametrize.json()["data"][0][key] == value 
+    response_get = requests.get(url =f'{URL}/trainers', params = {'trainer_id': TRAINER_ID})   
+    assert response_get.json()["data"][0]["trainer_name"] == 'ЛёхаЛепёха'
